1b5c233fb02a378dc8a6d93707935380e6cf765d
Valeria Argomedo <valeriaargomedo@gmail.com>
2021-10-28 11:10:16 -0300

    add library to use mapbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './styles/App.css';
 
 export default function App() {
-  const [data, setData] = useState();
-  fetch('https://fieldops-api.toroto.mx/api/projects')
-    .then((response) => response.json())
-    .then((info) =>  setData(info));
+  // const [data, setData] = useState();
+  // fetch('https://fieldops-api.toroto.mx/api/projects')
+  //   .then((response) => response.json())
+  //   .then((info) =>  setData(info));
   return <div>hello</div>;
 }
