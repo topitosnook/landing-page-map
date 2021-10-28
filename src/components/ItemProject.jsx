4d1ea9c1212369d6de975e30850b20011c2288cd
Valeria Argomedo <valeriaargomedo@gmail.com>
2021-10-28 10:20:32 -0300

    Add initial components

---
+++ src/components/ItemProject.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ItemProject() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
---
+++ src/components/SectionImage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SectionImage() {
+  //Hacer que se pueda poner una o mas de una... otro componente para cargar la imagen??
+  // Usar en mapa de obras y galeria de imagenes
+  return (
+    <div>
+      
+    </div>
+  );
+}
