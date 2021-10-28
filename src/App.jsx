7c34de8d75ad6341ddb5a0c2cd083051b6098ac0
Valeria Argomedo <valeriaargomedo@gmail.com>
2021-10-28 11:00:32 -0300

    fetch data frum url, fix modules problem from eslint

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './styles/App.css';
+
+export default function App() {
+  const [data, setData] = useState();
+  fetch('https://fieldops-api.toroto.mx/api/projects')
+    .then((response) => response.json())
+    .then((info) =>  setData(info));
+  return <div>hello</div>;
+}
--- src/components/SectionImage.jsx
+++ src/components/SectionImage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function SectionImage() {
   //Hacer que se pueda poner una o mas de una... otro componente para cargar la imagen??
   // Usar en mapa de obras y galeria de imagenes
+  // la galeria de las imagenes tiene un modal para verlas mas grandes
   return (
     <div>
       
