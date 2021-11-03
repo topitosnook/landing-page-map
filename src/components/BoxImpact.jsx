c1a5843a1adad85993dc426710273b8e5eb52a14
Valeria Argomedo <valeriaargomedo@gmail.com>
2021-11-03 12:16:08 -0300

    fix background of impacts

--- src/components/BoxImpact.jsx
+++ src/components/BoxImpact.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 export default function BoxImpact({ impact }) {
   return (
     <div className='impact__card'>
-      <div className='impact__card__background'>
-        <div className='impact__card__info'>
-          <p className='impact__card__name'> {impact.value}</p>
-          <p className='impact__card__value'>{impact.name}</p>
-        </div>
+      <div className='impact__card__background'></div>
+      <div className='impact__card__info'>
+        <p className='impact__card__name'> {impact.value}</p>
+        <p className='impact__card__value'>{impact.name}</p>
       </div>
     </div>
   );
